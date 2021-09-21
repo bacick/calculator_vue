56a1dfa6ad9537835036e1f9e977b9f3febdef4c
bacick <eshtin@gmail.com>
2021-09-21 22:19:53 +0300

    add keyboard, radio

--- calculator/src/components/calc.vue
+++ calculator/src/components/calc.vue
@@ -2,17 +2,38 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div class="main">
-      <input type="number" v-model.number="operand1" />
+      <input type="number" v-model.number="operand1" :disabled='activ' />
       <input type="number" v-model.number="operand2" />
       ={{ result }}
     </div>
-    <br />
-    <button @click='add(operand1, operand2)'>+</button>
-    <button @click='sub(operand1, operand2)'>-</button>
-    <button @click='mult(operand1, operand2)'>*</button>
-    <button @click='div(operand1, operand2)'>/</button>
-    <button @click='pow(operand1, operand2)'>**</button>
-    <button @click='mod(operand1, operand2)'>%</button>
+    <div class="error" v-if='error'>
+      {{ error }}
+    </div>
+    <div class="opirations" >
+      <button v-for='operator in operators'
+        @click='calculate(operator)'
+        :key='operator'
+        :title='operator'
+        :disabled="operand1 === '' || operand2 === ''">
+        {{ operator }}
+      </button>
+    </div>
+    <div class="check">
+      <input type="checkbox" id='keyboard' v-model='show' />
+      <label for="keyboard">Отобразить экранную клавиатуру</label>
+    </div>
+    <div class="keyboard" v-show='show'>
+      <button v-for='key in keys'
+      :key='key'>{{ key }}</button>
+    </div>
+    <div class="checkInput">
+      <input type="radio" id='operand#1' :value='false' name='input' v-mode.="act"/>
+      <label for="operand#1">Операнд 1</label>
+      {{act}}
+      <input type="radio" id='operand#2' :value='true' name='input' v-mode="act"/>
+      <label for="operand#2">Операнд 2</label>
+      {{act}}
+    </div>
   </div>
 </template>
 
@@ -27,31 +48,59 @@ export default {
       operand1: 0,
       operand2: 0,
       result: 0,
-      error: 'На ноль делить незя!'
+      error: '',
+      operators: ['+', '-', '*', '/', '**', '%'],
+      keys: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Del'],
+      show: false,
+      activ: true,
+      act: ''
     }
   },
   methods: {
-    add (operand1, operand2) {
-      this.result = operand1 + operand2
-    },
-    sub (operand1, operand2) {
-      this.result = operand1 - operand2
-    },
-    mult (operand1, operand2) {
-      this.result = operand1 * operand2
-    },
-    div (operand1, operand2) {
-      if (operand1 === 0 || operand2 === 0) {
-        this.result = this.error
-      } else {
-        this.result = operand1 / operand2
+    calculate (operator = '+') {
+      this.error = ''
+      switch (operator) {
+        case '+':
+          this.add()
+          break
+        case '-':
+          this.sub()
+          break
+        case '*':
+          this.mult()
+          break
+        case '/':
+          this.div()
+          break
+        case '**':
+          this.pow()
+          break
+        case '%':
+          this.mod()
+          break
       }
     },
-    pow (operand1, operand2) {
-      this.result = operand1 ** operand2
+    add () {
+      this.result = this.operand1 + this.operand2
     },
-    mod (operand1, operand2) {
-      this.result = operand1 % operand2
+    sub () {
+      this.result = this.operand1 - this.operand2
+    },
+    mult () {
+      this.result = this.operand1 * this.operand2
+    },
+    div () {
+      if (this.operand2 === 0) {
+        this.error = 'На ноль делить незя!!!'
+      } else {
+        this.result = this.operand1 / this.operand2
+      }
+    },
+    pow () {
+      this.result = this.operand1 ** this.operand2
+    },
+    mod () {
+      this.result = this.operand1 % this.operand2
     }
   }
 }
